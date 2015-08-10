8f93d5af61ab78fe9eec4bc075c7d5abc0780c70
Nicholas Schreiner <nicholas-schreiner@sbcglobal.net>
2015-08-10 01:19:11 -0500

    commit test - example functions

--- cube.py
+++ cube.py
@@ -2,3 +2,13 @@
 class Cube:
     def __init__(self):
         pass
+
+    def equals(self, other_cube):
+        pass
+    
+    def apply_move(self, move):
+        pass
+    
+    def apply_alg(self, alg):
+        pass
+    
