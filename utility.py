aff0c77c41c83e3d064ac1a60a92d2082b6560b1
Bryan Tremblay <bryanandhallie@yahoo.com>
2015-08-11 08:34:26 -0600

    Added a standard cube emulator written from scratch that will allow you to manipulate a cube of any magnitude in it's standard 3x3x3 configuration by passing standard algorithms to it. Not fully implemented as it currently does not recognize all commands such as wide u, d, l, r, f, b etc... but it works decently. Also made it render to console in color while emulating orange since DOS cannot actually render orange

---
+++ utility.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+import fixpath
+from colorama import *
+init(autoreset=True)
+
+from enum import Enum
+
+class Axis(Enum):
+    x, y, z = range(3)
+
+class Move(Enum):
+    U, D, L, R, F, B = range(6)
+
+class Modifier(Enum):
+    Normal, Twice, Prime = range(3)
+
+# Assumes array is square and not rectangular
+def rot2DArray(old_array):
+    magnitude = len(old_array[0])
+    new_array = []
+    for d in range(magnitude):
+        new_array.append(old_array[d][:])
+    for i in range(magnitude):
+        for j in range(magnitude):
+            new_array[j][magnitude - i - 1] = old_array[i][j]
+    return new_array
+
+# Standard DOS command prompt doesn't have Orange... so I had to cheat it in (Doesn't look perfect)
+def render_face(face):
+    backColor = Back.BLACK
+    foreColor = Fore.WHITE;
+    character = '\u2588'
+    if(face == "U"):foreColor = Fore.WHITE
+    elif(face == "D"):foreColor = Fore.YELLOW
+    elif(face == "F"):foreColor = Fore.GREEN
+    elif(face == "B"):foreColor = Fore.BLUE
+    elif(face == "L"):
+        foreColor = Fore.RED
+        backColor = Back.YELLOW
+        character = '\u2591'
+    elif(face == "R"):foreColor = Fore.RED
+    print(Style.BRIGHT + backColor + foreColor + character, end="")
+
+def stress_test(cube, alg, iterations):
+    import time
+    start_time = time.time()
+    for d in range(iterations):
+        cube.apply_alg(alg)
+    print("Took %s seconds too apply algorithm\n%s\n%i times" % ((time.time() - start_time), alg, iterations))
